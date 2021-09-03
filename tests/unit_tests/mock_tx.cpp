b3493028d493e161394c0267c132750f8b3696fc
koe <ukoe@protonmail.com>
2021-09-03 11:22:07 -0500

    add unit test

---
+++ tests/unit_tests/mock_tx.cpp
@@ -0,0 +1,178 @@
+// Copyright (c) 2021, The Monero Project
+// 
+// All rights reserved.
+// 
+// Redistribution and use in source and binary forms, with or without modification, are
+// permitted provided that the following conditions are met:
+// 
+// 1. Redistributions of source code must retain the above copyright notice, this list of
+//    conditions and the following disclaimer.
+// 
+// 2. Redistributions in binary form must reproduce the above copyright notice, this list
+//    of conditions and the following disclaimer in the documentation and/or other
+//    materials provided with the distribution.
+// 
+// 3. Neither the name of the copyright holder nor the names of its contributors may be
+//    used to endorse or promote products derived from this software without specific
+//    prior written permission.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
+// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
+// THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+// STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+// THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#include "crypto/crypto.h"
+#include "mock_tx/mock_rctclsag.h"
+#include "ringct/rctOps.h"
+#include "ringct/rctTypes.h"
+
+#include "gtest/gtest.h"
+
+#include <vector>
+
+
+enum class TestType
+{
+  ExpectTrue,
+  ExpectThrow
+};
+
+struct MockTxGenData
+{
+  std::size_t ref_set_size;
+  std::vector<rct::xmr_amount> input_amounts;
+  std::vector<rct::xmr_amount> output_amounts;
+  TestType expected_result;
+};
+
+void run_mock_tx_test(const std::vector<MockTxGenData> &gen_data)
+{
+  for (const auto &gen : gen_data)
+  {
+    // inputs
+    auto inputs{gen_mock_tx_clsag_inputs(gen.input_amounts, gen.ref_set_size)};
+
+    // destinations
+    auto destinations{gen_mock_tx_clsag_dests(gen.output_amounts)};
+
+    // make tx
+    mock_tx::MockTxCLSAG tx{inputs, destinations};
+
+    // validate tx
+    bool result{tx.validate()};
+
+    switch (gen.expected_result)
+    {
+      case (TestType::ExpectTrue):
+        EXPECT_TRUE(result);
+        break;
+      case (TestType::ExpectThrow):
+        EXPECT_THROW(result);
+        break;
+      default:
+        break;
+    };
+  }
+}
+
+
+TEST(mock_tx, clsag)
+{
+  /// success cases
+  std::vector<MockTxGenData> gen_data;
+  gen_data.resize(10);
+
+  // 1-in/1-out; ref set 1
+  gen_data[0].expected_result = TestType::ExpectTrue;
+  gen_data[0].input_amounts.push_back(1);
+  gen_data[0].output_amounts.push_back(1);
+  gen_data[0].ref_set_size = 1;
+
+  // 1-in/1-out; ref set 10
+  gen_data[1].expected_result = TestType::ExpectTrue;
+  gen_data[1].input_amounts.push_back(1);
+  gen_data[1].output_amounts.push_back(1);
+  gen_data[1].ref_set_size = 10;
+
+  // 1-in/2-out
+  gen_data[2].expected_result = TestType::ExpectTrue;
+  gen_data[2].input_amounts.push_back(2);
+  gen_data[2].output_amounts.push_back(1);
+  gen_data[2].output_amounts.push_back(1);
+  gen_data[2].ref_set_size = 10;
+
+  // 2-in/1-out
+  gen_data[3].expected_result = TestType::ExpectTrue;
+  gen_data[3].input_amounts.push_back(1);
+  gen_data[3].input_amounts.push_back(1);
+  gen_data[3].output_amounts.push_back(2);
+  gen_data[3].ref_set_size = 10;
+
+  // 16-in/16-out; ref set 1
+  gen_data[4].expected_result = TestType::ExpectTrue;
+  gen_data[4].ref_set_size = 1;
+  for (std::size_t i{0}; i < 16; ++16)
+  {
+    gen_data[4].input_amounts.push_back(1);
+    gen_data[4].output_amounts.push_back(1);
+  }
+
+  // 16-in/16-out; ref set 10
+  gen_data[5].expected_result = TestType::ExpectTrue;
+  gen_data[5].ref_set_size = 10;
+  for (std::size_t i{0}; i < 16; ++16)
+  {
+    gen_data[5].input_amounts.push_back(1);
+    gen_data[5].output_amounts.push_back(1);
+  }
+
+  // 16-in/16-out + amounts 0
+  gen_data[6].expected_result = TestType::ExpectTrue;
+  gen_data[6].ref_set_size = 10;
+  for (std::size_t i{0}; i < 16; ++16)
+  {
+    gen_data[6].input_amounts.push_back(0);
+    gen_data[6].output_amounts.push_back(0);
+  }
+
+  /// failure cases
+
+  // no inputs
+  gen_data[7].expected_result = TestType::ExpectThrow;
+  gen_data[7].output_amounts.push_back(0);
+  gen_data[7].ref_set_size = 10;
+
+  // no outputs
+  gen_data[8].expected_result = TestType::ExpectThrow;
+  gen_data[8].input_amounts.push_back(0);
+  gen_data[8].ref_set_size = 10;
+
+  // no ref set size
+  gen_data[9].expected_result = TestType::ExpectThrow;
+  gen_data[9].input_amounts.push_back(1);
+  gen_data[9].output_amounts.push_back(1);
+  gen_data[9].ref_set_size = 0;
+
+  // amounts don't balance
+  gen_data[10].expected_result = TestType::ExpectThrow;
+  gen_data[10].input_amounts.push_back(2);
+  gen_data[10].output_amounts.push_back(1);
+  gen_data[10].ref_set_size = 10;
+
+
+  /// run tests
+  run_mock_tx_test(gen_data);
+}
+
+
+
+
+
+
+
+
