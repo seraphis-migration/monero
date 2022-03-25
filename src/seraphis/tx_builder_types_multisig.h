f447dc3c7750275ced9ee5717f8ebcc881e8db06
koe <ukoe@protonmail.com>
2022-03-25 09:50:25 -0500

    work on seraphis multisig tx builder interface

--- src/seraphis/tx_builder_types_multisig.h
+++ src/seraphis/tx_builder_types_multisig.h
@@ -34,6 +34,7 @@
 
 //local headers
 #include "crypto/crypto.h"
+#include "multisig/multisig_account.h"
 #include "multisig/multisig_signer_set_filter.h"
 #include "ringct/rctTypes.h"
 #include "sp_core_types.h"
@@ -43,6 +44,7 @@
 //third party headers
 
 //standard headers
+#include <unordered_map>
 
 //forward declarations
 
@@ -59,6 +61,10 @@ struct SpMultisigInputProposalV1 final
     /// enote to spend
     SpEnoteV1 m_enote;
 
+    /// a: input amount
+    rct::xmr_amount m_input_amount;
+    /// x: input amount commitment's blinding factor
+    crypto::secret_key m_input_amount_blinding_factor;
     /// t_k
     crypto::secret_key m_address_mask;
     /// t_c
@@ -71,7 +77,7 @@ struct SpMultisigInputProposalV1 final
     * brief: get_key_image - get this input's key image
     * outparam: key_image_out - KI
     */
-    void get_key_image(crypto::key_image &key_image_out) const;
+    void get_key_image(const crypto::secret_key &enote_view_privkey, crypto::key_image &key_image_out) const;
 
     /**
     * brief: get_enote_core - get the enote this input proposal represents
@@ -98,6 +104,13 @@ struct SpMultisigInputProposalV1 final
     void gen(const rct::xmr_amount amount);
 };
 
+//temp
+void make_v1_multisig_input_proposal_v1(const SpEnoteV1 &enote,
+    const crypto::secret_key &address_mask,
+    const crypto::secret_key &commitment_mask,
+    SpMultisigInputProposalV1 &proposal_out);
+void make_v1_multisig_input_proposal_v1(const SpEnoteV1 &enote, SpMultisigInputProposalV1 &proposal_out);
+
 ////
 // SpMultisigTxProposalV1
 // - propose to fund a set of outputs with multisig inputs
@@ -113,40 +126,78 @@ struct SpMultisigTxProposalV1 final
     TxExtra m_partial_memo;
     /// tx inputs to sign with multisig
     std::vector<SpMultisigInputProposalV1> m_input_proposals;
-    /// multisig composition proof proposals for the proposed inputs
-    std::vector<SpCompositionProofMultisigProposal> m_multisig_input_proof_proposals;
+    /// composition proof proposals for each input proposal
+    std::vector<SpCompositionProofMultisigProposal> m_input_proof_proposals;
     /// all multisig signers who should participate in signing this proposal
     /// - the set may be larger than 'threshold', in which case every permutation of 'threshold' signers will attempt to sign
     multisig::signer_set_filter m_aggregate_signer_set_filter;
+
+    //todo: convert to plain tx proposal
+    void get_v1_tx_proposal_v1(SpTxProposalV1 &tx_proposal_out);
 };
+
+//temp
+void make_v1_multisig_tx_proposal_v1(JamtisPaymentProposalV1 explicit_payments,
+    SpOutputProposalV1 opaque_payments,
+    TxExtra partial_memo,
+    std::vector<SpMultisigInputProposalV1> input_proposals,
+    const multisig::signer_set_filter aggregate_signer_set_filter,
+    SpMultisigTxProposalV1 &proposal_out);
+void make_v1_multisig_tx_proposal_v1(JamtisPaymentProposalV1 explicit_payments,
+    SpOutputProposalV1 opaque_payments,
+    TxExtra partial_memo,
+    const std::vector<SpEnoteV1> &input_enote_proposals,
+    const std::vector<crypto::secret_key> &input_enote_view_privkeys,
+    const multisig::signer_set_filter aggregate_signer_set_filter,
+    SpMultisigTxProposalV1 &proposal_out);
 
 ////
 // SpMultisigInputInitV1
 // - initialize a seraphis composition proof for an enote image
-// - has multiple initializers for different sets of multisig signers
+// - has proof nonce pairs for multiple sets of multisig signers (represented by an aggregate filter)
+// - only signer sets that include 'signer_id' will be initialized
 ///
 struct SpMultisigInputInitV1 final
 {
+    /// id of signer who made this input initializer
+    crypto::public_key m_signer_id;
     /// proposal prefix (represents the set of destinations and memos; will be signed by this input's image proof)
     rct::key m_proposal_prefix;
     /// key image of the enote image this initializer corresponds to (for tracking)
     crypto::key_image m_key_image;
 
-    /// sets of multisig signers this initializer lines up with (an aggregate representation)
+    /// all multisig signers who should participate in attempting to make this composition proof
     multisig::signer_set_filter m_aggregate_signer_set_filter;
 
-    /// signature nonce pubkeys for each signer set
+    /// signature nonce pubkeys for each signer set that includes the specified signer id
+    /// - all permutations of the aggregate filter that don't include the signer id are ignored
     // alpha_{ki,1,e}*U
     std::vector<rct::key> signature_nonce_1_KI_pub;
     // alpha_{ki,2,e}*U
     std::vector<rct::key> signature_nonce_2_KI_pub;
 };
 
+//temp
+void make_v1_multisig_input_init_v1(const crypto::public_key &signer_id,
+    const std::vector<crypto::public_key> &multisig_signers,
+    const std::uint32_t threshold,
+    const rct::key &proposal_prefix,
+    const crypto::key_image &key_image,
+    const multisig::signer_set_filter aggregate_signer_set_filter,
+    SpCompositionProofMultisigNonceRecord &nonce_record_inout,
+    SpMultisigInputInitV1 &input_init_out);
+void make_v1_multisig_input_inits_v1(const crypto::public_key &signer_id,
+    const std::vector<crypto::public_key> &multisig_signers,
+    const std::uint32_t threshold,
+    const SpMultisigTxProposalV1 &tx_proposal,
+    SpCompositionProofMultisigNonceRecord &nonce_record_inout,
+    std::vector<SpMultisigInputInitV1> &input_inits_out);
+
 ////
-// SpMultisigInputResponseV1
+// SpMultisigInputPartialSigV1
 // - partially signed input; combine partial signatures to complete the image proof for a partial input
 ///
-struct SpMultisigInputResponseV1 final
+struct SpMultisigInputPartialSigV1 final
 {
     /// proposal prefix (represents the set of destinations and memos; signed by this composition proof)
     rct::key m_proposal_prefix;
@@ -159,5 +210,36 @@ struct SpMultisigInputResponseV1 final
     /// set of multisig signers this partial signature corresponds to
     multisig::signer_set_filter m_signer_set_filter;
 };
+
+//static std::unordered_map<crypto::key_image, std::vector<SpMultisigInputInitV1>> organize_by_key_image(
+//    std::vector<SpMultisigInputInitV1> input_inits);
+
+//temp
+// - should be 'loose': make as many responses as possible, ignore signer sets that don't have nonces in the record
+//   (in case earlier responses removed nonces from the record)
+void make_v1_multisig_input_partial_sig_v1(const multisig::multisig_account &signer_account,
+    const SpMultisigInputProposalV1 &input_proposal,
+    const crypto::secret_key &input_enote_view_privkey,
+    const rct::key &proposal_prefix,
+    const multisig::signer_set_filter signer_set_filter,
+    SpCompositionProofMultisigNonceRecord &nonce_record_inout,
+    SpMultisigInputPartialSigV1 &input_partial_sig_out);
+void make_v1_multisig_input_partial_sigs_single_input_v1(const multisig::multisig_account &signer_account,
+    const SpMultisigInputProposalV1 &input_proposal,
+    const crypto::secret_key &input_enote_view_privkey,
+    const std::vector<SpMultisigInputInitV1> &input_inits,  //including from self
+    SpCompositionProofMultisigNonceRecord &nonce_record_inout,
+    std::vector<SpMultisigInputPartialSigV1> &input_partial_sigs_out);
+void make_v1_multisig_input_partial_sigs_multiple_inputs_v1(const multisig::multisig_account &signer_account,
+    const std::vector<SpMultisigInputProposalV1> &input_proposals,
+    const std::unordered_map<crypto::key_image, crypto::secret_key> &input_enote_view_privkeys,
+    const std::vector<SpMultisigInputInitV1> &input_inits,
+    SpCompositionProofMultisigNonceRecord &nonce_record_inout,
+    std::unordered_map<crypto::key_image, std::vector<SpMultisigInputPartialSigV1>> &input_partial_sigs_out);
+
+void make_v1_partial_input_v1(const SpMultisigInputProposalV1 &input_proposal,
+    const crypto::secret_key &input_enote_view_privkey,  //use to verify that key images match for 'correct by construction'
+    const std::vector<SpMultisigInputPartialSigV1> &input_partial_sigs,
+    SpPartialInputV1 &partial_input_out);
 
 } //namespace sp
