3bdfd68ad04e7e2bc2464ce61243e8e924bf063d
DangerousFreedom <monero-inflation-checker@protonmail.com>
2023-09-19 22:50:04 +0200

    mark temporary functions/remove duplicated io test

--- src/seraphis_wallet/transaction_utils.h
+++ src/seraphis_wallet/transaction_utils.h
@@ -26,6 +26,9 @@
 // STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
 // THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+// ********* TEMPORARY FILE *********
+// Jamtis checksum will probably change its place
+
 #pragma once
 
 // local headers
