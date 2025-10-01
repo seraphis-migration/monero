f57a41e9115d4abf15376b88f129bd8898d016f2
j-berman <justinberman@protonmail.com>
2025-10-01 12:13:58 -0700

    Rm version validity check for stressnet

--- src/daemon/rpc_command_executor.cpp
+++ src/daemon/rpc_command_executor.cpp
@@ -2436,7 +2436,7 @@ bool t_rpc_command_executor::version()
         }
     }
 
-    if (res.version.empty() || !cryptonote::rpc::is_version_string_valid(res.version))
+    if (res.version.empty()/* || !cryptonote::rpc::is_version_string_valid(res.version) */)
     {
         tools::fail_msg_writer() << "The daemon software version is not available.";
     }
